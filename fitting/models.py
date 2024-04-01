0177dc038b9c07e6fa872826270e4633bff11798
Adrian Crawford <pnr5sh@virginia.edu>
2024-04-01 16:50:50 -0400

    added axes labels & created run.py for testing

--- fitting/models.py
+++ fitting/models.py
@@ -508,14 +508,16 @@ class GP3D(GP):
 
         if plot:
             ax.invert_yaxis()
+            ax.set_xlabel('Normalized Time [days]')
+            ax.set_ylabel('Normalized Magnitude')
             plt.legend()
             plt.show()
 
 
 
-kernel = RBFKernel([np.log(10.0), 500.0], (0.1, 2.0e3)).kernel + WhiteNoiseKernel(1., (1e-10, 10.)).kernel
-#kernel = Matern([10.0, 500.0], (1., 2.0e3), 2.5)
-gp = GP3D('SNII', 'SNIIP', kernel)
-gp.predict_gp(['UVW2', 'UVM2', 'UVW1', 'U', 'B', 'V'], -20, 50, 0.9, plot=True, log_transform=30)
+# kernel = RBFKernel([np.log(10.0), 500.0], (0.1, 2.0e3)).kernel + WhiteNoiseKernel(1., (1e-10, 10.)).kernel
+# #kernel = Matern([10.0, 500.0], (1., 2.0e3), 2.5)
+# gp = GP3D('SNII', 'SNIIP', kernel)
+# gp.predict_gp(['UVW2', 'UVM2', 'UVW1', 'U', 'B', 'V'], -20, 50, 0.9, plot=True, log_transform=30)
 
-print(gp.gaussian_process.kernel_)
+# print(gp.gaussian_process.kernel_)
